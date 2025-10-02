adf398b66d388615246bbfffc02966ec8618728e
Martin Grünwald-Schreiber <34436146+grunwmar@users.noreply.github.com>
2025-10-02 21:03:00 +0200

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cd $HOME/.local/auto_sshfs
+
+fatal() {
+    echo "FATAL ERROR: $@"
+    exit 1
+  }
+  trap 'sudo -k' EXIT
+  zenity --password | sudo -Sv || fatal "Unable to sudo"
+
 ./allow_run_media_mountpoint.sh
 ./venv/bin/python __main__.py $1
 
